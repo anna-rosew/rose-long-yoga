c1a7ec6d8de6e11851aa89173e97e5ce628a7986
Anna <annarosewain9@gmail.com>
2024-10-09 11:22:09 +0100

    Adding new stylesheet to Video blog content

--- src/pages/BlogPosts/LynnInterview.js
+++ src/pages/BlogPosts/LynnInterview.js
@@ -1,26 +1,27 @@
 import React from "react";
-import "../../styles/App.css";
-import "../../styles/Article.css";
-import arrowIcon from "../../styles/imgs/icons/blog-arrow.svg";
 import { Link } from "react-router-dom";
+
+import "../../styles/partials/_blog-content.scss";
 
 export default function LynnInterview() {
   return (
     <div className="VideoTemp">
       <div className="container">
         <div className="blog-article">
-          <h3 className="subtitle">
-            <strong>VIDEO: </strong>INTERVIEW
-          </h3>
-          <h1>Mandalas, Yantras & Sacred Geometry with Lynn Hanford-Day</h1>
-          <p>
-            <strong>22 AUG 2024</strong>
-          </p>
-          <p className="article-summary">
-            Join Rose Long in conversation with visual artist Lynn Handford-Day,
-            expanding on the ancient practices behind her deeply intricate work
-            and the stories at the root of her practice.
-          </p>
+          <div className="article-header">
+            <h3 className="subtitle">
+              <strong>VIDEO: </strong>INTERVIEW
+            </h3>
+            <h1>Mandalas, Yantras & Sacred Geometry with Lynn Hanford-Day</h1>
+            <p>
+              <strong>22 AUG 2024</strong>
+            </p>
+            <p className="article-summary">
+              Join Rose Long in conversation with visual artist Lynn
+              Handford-Day, expanding on the ancient practices behind her deeply
+              intricate work and the stories at the root of her practice.
+            </p>
+          </div>
           <div className="player-wrapper">
             <iframe
               src="https://player.vimeo.com/video/1001545755?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
@@ -43,12 +44,11 @@ export default function LynnInterview() {
             </a>{" "}
           </p>
         </div>{" "}
-        <div className="article-bottom-buttons">
-          <Link to="/SelfArticle" className="back-button">
+        <div className="blog-bottom-buttons">
+          <Link to="/SelfArticle" className="blog-button" role="button">
             MORE CONTENT
-            <img src={arrowIcon} className="blog-arrow" alt="Arrow" />
           </Link>
-          <Link to="/work" className="journey-button">
+          <Link to="/work" className="blog-button" role="button">
             START YOUR JOURNEY
           </Link>
         </div>
