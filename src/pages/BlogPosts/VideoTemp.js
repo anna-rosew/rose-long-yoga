dcd1eec82ab6fc10ea225d7743a4911b551ad893
Anna <annarosewain9@gmail.com>
2024-10-10 15:28:39 +0100

    Editing Blog video template page

--- src/pages/BlogPosts/VideoTemp.js
+++ src/pages/BlogPosts/VideoTemp.js
@@ -1,44 +1,63 @@
-import React from "react";
-
-import "../../styles/Article.css";
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function VideoTemp() {
+import "../../styles/partials/_blog-content.scss";
+import transriptIcon from "../../styles/imgs/icons/transcript.png";
+import captionsIcon from "../../styles/imgs/icons/captions.png";
+
+export default function BlogVideoTemplate() {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <div className="container">
-      <article className="blog-article video">
-        <h3 className="subtitle">
-          <strong>VIDEO: </strong>INTERVIEW
-        </h3>
-        <h1>Mandalas, Yantras & Sacred Geometry with Lynn Hanford-Day</h1>
-        <p>
-          Published on:<strong>22 AUG 2024</strong>
-        </p>
-        <p className="article-summary">
-          Join Rose Long in conversation with visual artist Lynn Handford-Day,
-          expanding on the ancient practices behind her deeply intricate work
-          and the stories at the root of her practice.
-        </p>
-        <div className="player-wrapper">
-          <iframe
-            src="https://player.vimeo.com/video/1001545755?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            width="960"
-            height="540"
-            frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowfullscreen
-            title="vimeo player"
-          ></iframe>
+    <div className="BlogVideo">
+      <div className="container">
+        <div className="blog-article">
+          <div className="article-header">
+            <h4 className="subtitle">
+              <strong>VIDEO: </strong>INTERVIEW
+            </h4>
+            <h1>Mandalas, Yantras & Sacred Geometry with Lynn Hanford-Day</h1>
+            <h4 className="date">08/24</h4>
+            <p className="article-summary">
+              Join Rose Long in conversation with visual artist Lynn
+              Handford-Day, expanding on the ancient practices behind her deeply
+              intricate work and the stories at the root of her practice.
+            </p>
+          </div>
+          <div className="player-wrapper">
+            <iframe
+              src="https://player.vimeo.com/video/1001545755?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+              width="960"
+              height="540"
+              frameborder="0"
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowfullscreen
+              title="vimeo player"
+            ></iframe>
+          </div>
+          <div className="accessibility-buttons">
+            <div className="accessibility-video-button" role="button">
+              <img className="icon" src={transriptIcon} alt="transcript-icon" />
+              TRANSCRIPT
+            </div>
+            <div
+              className="accessibility-video-button"
+              role="button"
+              onClick={() => setOpenModal(true)}
+            >
+              <img className="icon" src={captionsIcon} alt="transcript-icon" />
+              CAPTIONS
+            </div>
+            {openModal && <CaptionsModal closeModal={setOpenModal} />}
+          </div>
+        </div>{" "}
+        <div className="blog-bottom-buttons">
+          <Link to="/SelfArticle" className="blog-button" role="button">
+            MORE CONTENT
+          </Link>
+          <Link to="/work" className="blog-button inverse-button" role="button">
+            START YOUR JOURNEY
+          </Link>
         </div>
-      </article>
-      <div className="blog-bottom-buttons">
-        <Link to="/SelfArticle" className="blog-button" role="button">
-          MORE CONTENT
-        </Link>
-        <Link to="/work" className="blog-button" role="button">
-          START YOUR JOURNEY
-        </Link>
       </div>
     </div>
   );
